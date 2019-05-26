6057d0d9c0a82b71579339c22b28f92c6aa17518
Zumanzi <50646077+Zumanzi@users.noreply.github.com>
2019-05-26 02:58:15 -0500

    Add files via upload
    IDS 455 Final Project-Map Visualizations

---
+++ Fast Food.com/map-config.js
@@ -0,0 +1,317 @@
+﻿var usjsconfig = {
+  "usjs1":{
+    "hover": "Alabama 6.3",//info of the popup
+    "url": "https://www.businessinsider.com/the-most-popular-fast-food-restaurants-in-america-2012-7",
+    "target": "same_window",// use "new_window", "same_window", "modal", or "none"
+    "upColor": "#7B68EE",//default color-BLUE
+    "overColor": "#ECFFB3",//highlight color
+    "downColor": "#cae9af",//clicking color
+    "active": true//true/false to activate/deactivate
+  },
+  "usjs2":{
+    "hover": "ALASKA 2.6",
+    "url": "https://www.businessinsider.com/the-most-popular-fast-food-restaurants-in-america-2012-7", "target": "same_window",
+    "upColor": "#FFFF00", "overColor": "#d9ff66", "downColor": "#cae9af",
+    "active": true
+  },
+  "usjs3":{
+    "hover": "ARIZONA 4.6",
+    "url": "https://www.businessinsider.com/the-most-popular-fast-food-restaurants-in-america-2012-7", "target": "same_window",
+    "upColor": "#7B68EE", "overColor": "#beef2a", "downColor": "#cae9af",
+    "active": true
+  },
+  "usjs4":{
+    "hover": "ARKANSAS 4.0",
+    "url": "https://www.businessinsider.com/the-most-popular-fast-food-restaurants-in-america-2012-7", "target": "same_window",
+    "upColor": "#7B68EE", "overColor": "#d9ff66", "downColor": "#cae9af",
+    "active": true
+  },
+  "usjs5":{
+    "hover": "CALIFORNIA 3.6",
+    "url": "https://www.businessinsider.com/the-most-popular-fast-food-restaurants-in-america-2012-7", "target": "same_window",
+    "upColor": "#800080", "overColor": "#cbe600", "downColor": "#cae9af",
+    "active": true
+  },
+  "usjs6":{
+    "hover": "COLORADO 4.0",
+    "url": "https://www.businessinsider.com/the-most-popular-fast-food-restaurants-in-america-2012-7", "target": "same_window",
+    "upColor": "#800080", "overColor": "#ECFFB3", "downColor": "#cae9af",
+    "active": true
+  },
+  "usjs7":{
+    "hover": "CONNECTICUT 2.4",
+    "url": "https://www.businessinsider.com/the-most-popular-fast-food-restaurants-in-america-2012-7", "target": "same_window",
+    "upColor": "#FFFF00", "overColor": "#ECFFB3", "downColor": "#cae9af",
+    "active": true
+  },
+  "usjs8":{
+    "hover": "DELAWARE 4.1",
+    "url": "https://www.businessinsider.com/the-most-popular-fast-food-restaurants-in-america-2012-7", "target": "same_window",
+    "upColor": "#800080", "overColor": "#ECFFB3", "downColor": "#cae9af",
+    "active": true
+  },
+  "usjs9":{
+    "hover": "FLORIDA 3.3",
+    "url": "https://www.businessinsider.com/the-most-popular-fast-food-restaurants-in-america-2012-7", "target": "same_window",
+    "upColor": "#FFFF00", "overColor": "#ECFFB3", "downColor": "#cae9af",
+    "active": true
+  },
+  "usjs10":{
+    "hover": "GEORGIA 4.9",
+    "url": "https://www.businessinsider.com/the-most-popular-fast-food-restaurants-in-america-2012-7", "target": "same_window",
+    "upColor": "#7B68EE", "overColor": "#ECFFB3", "downColor": "#cae9af",
+    "active": true
+  },
+  "usjs11":{
+    "hover": "HAWAII 3.2",
+    "url": "https://www.businessinsider.com/the-most-popular-fast-food-restaurants-in-america-2012-7", "target": "same_window",
+    "upColor": "#FFFF00", "overColor": "#ECFFB3", "downColor": "#cae9af",
+    "active": true
+  },
+  "usjs12":{
+    "hover": "IDAHO 3.8",
+    "url": "https://www.businessinsider.com/the-most-popular-fast-food-restaurants-in-america-2012-7", "target": "same_window",
+    "upColor": "#800080", "overColor": "#ECFFB3", "downColor": "#cae9af",
+    "active": true
+  },
+  "usjs13":{
+    "hover": "ILLINOIS 4.5",
+    "url": "https://www.businessinsider.com/the-most-popular-fast-food-restaurants-in-america-2012-7", "target": "same_window",
+    "upColor": "#7B68EE", "overColor": "#d9ff66", "downColor": "#cae9af",
+    "active": true
+  },
+  "usjs14":{
+    "hover": "INDIANA 5.0",
+    "url": "https://www.businessinsider.com/the-most-popular-fast-food-restaurants-in-america-2012-7", "target": "same_window",
+    "upColor": "#7B68EE", "overColor": "#ECFFB3", "downColor": "#cae9af",
+    "active": true
+  },
+  "usjs15":{
+    "hover": "IOWA 4.7",
+    "url": "https://www.businessinsider.com/the-most-popular-fast-food-restaurants-in-america-2012-7", "target": "same_window",
+    "upColor": "#7B68EE", "overColor": "#ECFFB3", "downColor": "#cae9af",
+    "active": true
+  },
+  "usjs16":{
+    "hover": "KANSAS 4.7",
+    "url": "https://www.businessinsider.com/the-most-popular-fast-food-restaurants-in-america-2012-7", "target": "same_window",
+    "upColor": "#7B68EE", "overColor": "#ECFFB3", "downColor": "#cae9af",
+    "active": true
+  },
+  "usjs17":{
+    "hover": "KENTUCKY 4.8",
+    "url": "https://www.businessinsider.com/the-most-popular-fast-food-restaurants-in-america-2012-7", "target": "same_window",
+    "upColor": "#7B68EE", "overColor": "#ECFFB3", "downColor": "#cae9af",
+    "active": true
+  },
+  "usjs18":{
+    "hover": "LOUISIANA 4.4",
+    "url": "https://www.businessinsider.com/the-most-popular-fast-food-restaurants-in-america-2012-7", "target": "same_window",
+    "upColor": "#800080", "overColor": "#ECFFB3", "downColor": "#cae9af",
+    "active": true
+  },
+  "usjs19":{
+    "hover": "MAINE 3.1",
+    "url": "https://www.businessinsider.com/the-most-popular-fast-food-restaurants-in-america-2012-7", "target": "same_window",
+    "upColor": "#FFFF00", "overColor": "#d9ff66", "downColor": "#cae9af",
+    "active": true
+  },
+  "usjs20":{
+    "hover": "MARYLAND 3.9",
+    "url": "https://www.businessinsider.com/the-most-popular-fast-food-restaurants-in-america-2012-7", "target": "same_window",
+    "upColor": "#800080", "overColor": "#ECFFB3", "downColor": "#cae9af",
+    "active": true
+  },
+  "usjs21":{
+    "hover": "MASSACHUSETTS 3.1",
+    "url": "https://www.businessinsider.com/the-most-popular-fast-food-restaurants-in-america-2012-7", "target": "same_window",
+    "upColor": "#FFFF00", "overColor": "#ECFFB3", "downColor": "#cae9af",
+    "active": true
+  },
+  "usjs22":{
+    "hover": "MICHIGAN 4.0",
+    "url": "https://www.businessinsider.com/the-most-popular-fast-food-restaurants-in-america-2012-7", "target": "same_window",
+    "upColor": "#800080", "overColor": "#ECFFB3", "downColor": "#cae9af",
+    "active": true
+  },
+  "usjs23":{
+    "hover": "MINNESOTA 3.7",
+    "url": "https://www.businessinsider.com/the-most-popular-fast-food-restaurants-in-america-2012-7", "target": "same_window",
+    "upColor": "#800080", "overColor": "#ECFFB3", "downColor": "#cae9af",
+    "active": true
+  },
+  "usjs24":{
+    "hover": "MISSISSIPPI 2.1",
+    "url": "https://www.businessinsider.com/the-most-popular-fast-food-restaurants-in-america-2012-7", "target": "same_window",
+    "upColor": "#FFFF00", "overColor": "#ECFFB3", "downColor": "#cae9af",
+    "active": true
+  },
+  "usjs25":{
+    "hover": "MISSOURI 4.9",
+    "url": "https://www.businessinsider.com/the-most-popular-fast-food-restaurants-in-america-2012-7", "target": "same_window",
+    "upColor": "#7B68EE", "overColor": "#d9ff66", "downColor": "#cae9af",
+    "active": true
+  },
+  "usjs26":{
+    "hover": "MONTANA 3.8",
+    "url": "https://www.businessinsider.com/the-most-popular-fast-food-restaurants-in-america-2012-7", "target": "same_window",
+    "upColor": "#800080", "overColor": "#ECFFB3", "downColor": "#cae9af",
+    "active": true
+  },
+  "usjs27":{
+    "hover": "NEBRASKA 5.4",
+    "url": "https://www.businessinsider.com/the-most-popular-fast-food-restaurants-in-america-2012-7", "target": "same_window",
+    "upColor": "#7B68EE", "overColor": "#ECFFB3", "downColor": "#cae9af",
+    "active": true
+  },
+  "usjs28":{
+    "hover": "NEVADA 4.7",
+    "url": "https://www.businessinsider.com/the-most-popular-fast-food-restaurants-in-america-2012-7", "target": "same_window",
+    "upColor": "#7B68EE", "overColor": "#ECFFB3", "downColor": "#cae9af",
+    "active": true
+  },
+  "usjs29":{
+    "hover": "NEW HAMPSHIRE 3.2",
+    "url": "https://www.businessinsider.com/the-most-popular-fast-food-restaurants-in-america-2012-7", "target": "same_window",
+    "upColor": "#FFFF00", "overColor": "#ECFFB3", "downColor": "#cae9af",
+    "active": true
+  },
+  "usjs30":{
+    "hover": "NEW JERSEY 2.0",
+    "url": "https://www.businessinsider.com/the-most-popular-fast-food-restaurants-in-america-2012-7", "target": "same_window",
+    "upColor": "#FFFF00", "overColor": "#ECFFB3", "downColor": "#cae9af",
+    "active": true
+  },
+  "usjs31":{
+    "hover": "NEW MEXICO 4.3",
+    "url": "https://www.businessinsider.com/the-most-popular-fast-food-restaurants-in-america-2012-7", "target": "same_window",
+    "upColor": "#800080", "overColor": "#d9ff66", "downColor": "#cae9af",
+    "active": true
+  },
+  "usjs32":{
+    "hover": "NEW YORK 2.1",
+    "url": "https://www.businessinsider.com/the-most-popular-fast-food-restaurants-in-america-2012-7", "target": "same_window",
+    "upColor": "#FFFF00", "overColor": "#ECFFB3", "downColor": "#cae9af",
+    "active": true
+  },
+  "usjs33":{
+    "hover": "NORTH CAROLINA 4.5",
+    "url": "https://www.businessinsider.com/the-most-popular-fast-food-restaurants-in-america-2012-7", "target": "same_window",
+    "upColor": "#7B68EE", "overColor": "#ECFFB3", "downColor": "#cae9af",
+    "active": true
+  },
+  "usjs34":{
+    "hover": "NORTH DAKOTA 3.6",
+    "url": "https://www.businessinsider.com/the-most-popular-fast-food-restaurants-in-america-2012-7", "target": "same_window",
+    "upColor": "#800080", "overColor": "#ECFFB3", "downColor": "#cae9af",
+    "active": true
+  },
+  "usjs35":{
+    "hover": "OHIO 4.7",
+    "url": "https://www.businessinsider.com/the-most-popular-fast-food-restaurants-in-america-2012-7", "target": "same_window",
+    "upColor": "#7B68EE", "overColor": "#ECFFB3", "downColor": "#cae9af",
+    "active": true
+  },
+  "usjs36":{
+    "hover": "OKLAHOMA 5.3",
+    "url": "https://www.businessinsider.com/the-most-popular-fast-food-restaurants-in-america-2012-7", "target": "same_window",
+    "upColor": "#7B68EE", "overColor": "#ECFFB3", "downColor": "#cae9af",
+    "active": true
+  },
+  "usjs37":{
+    "hover": "OREGON 3.6",
+    "url": "https://www.businessinsider.com/the-most-popular-fast-food-restaurants-in-america-2012-7", "target": "same_window",
+    "upColor": "#800080", "overColor": "#ECFFB3", "downColor": "#cae9af",
+    "active": true
+  },
+  "usjs38":{
+    "hover": "PENNSYLVANIA 3.2",
+    "url": "https://www.businessinsider.com/the-most-popular-fast-food-restaurants-in-america-2012-7", "target": "same_window",
+    "upColor": "#FFFF00", "overColor": "#d9ff66", "downColor": "#cae9af",
+    "active": true
+  },
+  "usjs39":{
+    "hover": "RHODE ISLAND 2.5",
+    "url": "https://www.businessinsider.com/the-most-popular-fast-food-restaurants-in-america-2012-7", "target": "same_window",
+    "upColor": "#FFFF00", "overColor": "#ECFFB3", "downColor": "#cae9af",
+    "active": true
+  },
+  "usjs40":{
+    "hover": "SOUTH CAROLINA 4.9",
+    "url": "https://www.businessinsider.com/the-most-popular-fast-food-restaurants-in-america-2012-7", "target": "same_window",
+    "upColor": "#7B68EE", "overColor": "#ECFFB3", "downColor": "#cae9af",
+    "active": true
+  },
+  "usjs41":{
+    "hover": "SOUTH DAKOTA 4.3",
+    "url": "https://www.businessinsider.com/the-most-popular-fast-food-restaurants-in-america-2012-7", "target": "same_window",
+    "upColor": "#800080", "overColor": "#ECFFB3", "downColor": "#cae9af",
+    "active": true
+  },
+  "usjs42":{
+    "hover": "TENNESSEE 5.2",
+    "url": "https://www.businessinsider.com/the-most-popular-fast-food-restaurants-in-america-2012-7", "target": "same_window",
+    "upColor": "#7B68EE", "overColor": "#d9ff66", "downColor": "#cae9af",
+    "active": true
+  },
+  "usjs43":{
+    "hover": "TEXAS 4.3",
+    "url": "https://www.businessinsider.com/the-most-popular-fast-food-restaurants-in-america-2012-7", "target": "same_window",
+    "upColor": "#800080", "overColor": "#ECFFB3", "downColor": "#cae9af",
+    "active": true
+  },
+  "usjs44":{
+    "hover": "UTAH 3.6",
+    "url": "https://www.businessinsider.com/the-most-popular-fast-food-restaurants-in-america-2012-7", "target": "same_window",
+    "upColor": "#800080", "overColor": "#ECFFB3", "downColor": "#cae9af",
+    "active": true
+  },
+  "usjs45":{
+    "hover": "VERMONT 1.9",
+    "url": "https://www.businessinsider.com/the-most-popular-fast-food-restaurants-in-america-2012-7", "target": "same_window",
+    "upColor": "#FFFF00", "overColor": "#ECFFB3", "downColor": "#cae9af",
+    "active": true
+  },
+  "usjs46":{
+    "hover": "VIRGINIA 4.3",
+    "url": "https://www.businessinsider.com/the-most-popular-fast-food-restaurants-in-america-2012-7", "target": "same_window",
+    "upColor": "#800080", "overColor": "#ECFFB3", "downColor": "#cae9af",
+    "active": true
+  },
+  "usjs47":{
+    "hover": "WASHINGTON 3.1",
+    "url": "https://www.businessinsider.com/the-most-popular-fast-food-restaurants-in-america-2012-7", "target": "same_window",
+    "upColor": "#FFFF00", "overColor": "#ECFFB3", "downColor": "#cae9af",
+    "active": true
+  },
+  "usjs48":{
+    "hover": "WEST VIRGINIA 5.3",
+    "url": "https://www.businessinsider.com/the-most-popular-fast-food-restaurants-in-america-2012-7", "target": "same_window",
+    "upColor": "#7B68EE", "overColor": "#ECFFB3", "downColor": "#cae9af",
+    "active": true
+  },
+  "usjs49":{
+    "hover": "WISCONSIN 4.0",
+    "url": "https://www.businessinsider.com/the-most-popular-fast-food-restaurants-in-america-2012-7", "target": "same_window",
+    "upColor": "#800080", "overColor": "#ECFFB3", "downColor": "#cae9af",
+    "active": true
+  },
+  "usjs50":{
+    "hover": "WYOMING 4.1",
+    "url": "https://www.businessinsider.com/the-most-popular-fast-food-restaurants-in-america-2012-7", "target": "same_window",
+    "upColor": "#800080", "overColor": "#ECFFB3", "downColor": "#cae9af",
+    "active": true
+  },
+  "usjs51":{
+    "hover": "WASHINGTON DC 5.0",
+    "url": "https://www.businessinsider.com/the-most-popular-fast-food-restaurants-in-america-2012-7", "target": "same_window",
+    "upColor": "#7B68EE", "overColor": "#ECFFB3", "downColor": "#cae9af",
+    "active": true
+  },
+  "general":{
+    "borderColor": "#9CA8B6",
+    "visibleNames": "#adadad",
+    "lakesFill": "#C5E8FF",
+    "lakesOutline": "#9CA8B6"
+  }
+};
---
+++ Obesity Map.com/map-config.js
@@ -0,0 +1,317 @@
+﻿var usjsconfig = {
+  "usjs1":{
+    "hover": "Alabama 35.6%",//info of the popup
+    "url": "https://www.good.is/articles/mapping-america-s-eating-habits",
+    "target": "same_window",// use "new_window", "same_window", "modal", or "none"
+    "upColor": "#850d04",//default color-RED
+    "overColor": "#ECFFB3",//highlight color
+    "downColor": "#cae9af",//clicking color
+    "active": true//true/false to activate/deactivate
+  },
+  "usjs2":{
+    "hover": "ALASKA 29.8%",
+    "url": "https://www.good.is/articles/mapping-america-s-eating-habits", "target": "same_window",
+    "upColor": "#c56114", "overColor": "#d9ff66", "downColor": "#cae9af",
+    "active": true
+  },
+  "usjs3":{
+    "hover": "ARIZONA 28.4%",
+    "url": "https://www.good.is/articles/mapping-america-s-eating-habits", "target": "same_window",
+    "upColor": "#c56114", "overColor": "#beef2a", "downColor": "#cae9af",
+    "active": true
+  },
+  "usjs4":{
+    "hover": "ARKANSAS 34.5%",
+    "url": "https://www.good.is/articles/mapping-america-s-eating-habits", "target": "same_window",
+    "upColor": "#850d04", "overColor": "#d9ff66", "downColor": "#cae9af",
+    "active": true
+  },
+  "usjs5":{
+    "hover": "CALIFORNIA 24.2%",
+    "url": "https://www.good.is/articles/mapping-america-s-eating-habits", "target": "same_window",
+    "upColor": "#467706", "overColor": "#cbe600", "downColor": "#cae9af",
+    "active": true
+  },
+  "usjs6":{
+    "hover": "COLORADO 20.2%",
+    "url": "https://www.good.is/articles/mapping-america-s-eating-habits", "target": "same_window",
+    "upColor": "#467706", "overColor": "#ECFFB3", "downColor": "#cae9af",
+    "active": true
+  },
+  "usjs7":{
+    "hover": "CONNECTICUT 25.3%",
+    "url": "https://www.good.is/articles/mapping-america-s-eating-habits", "target": "same_window",
+    "upColor": "#c56114", "overColor": "#ECFFB3", "downColor": "#cae9af",
+    "active": true
+  },
+  "usjs8":{
+    "hover": "DELAWARE 29.7%",
+    "url": "https://www.good.is/articles/mapping-america-s-eating-habits", "target": "same_window",
+    "upColor": "#c56114", "overColor": "#ECFFB3", "downColor": "#cae9af",
+    "active": true
+  },
+  "usjs9":{
+    "hover": "FLORIDA 26.8%",
+    "url": "https://www.good.is/articles/mapping-america-s-eating-habits", "target": "same_window",
+    "upColor": "#c56114", "overColor": "#ECFFB3", "downColor": "#cae9af",
+    "active": true
+  },
+  "usjs10":{
+    "hover": "GEORGIA 30.7%",
+    "url": "https://www.good.is/articles/mapping-america-s-eating-habits", "target": "same_window",
+    "upColor": "#850d04", "overColor": "#ECFFB3", "downColor": "#cae9af",
+    "active": true
+  },
+  "usjs11":{
+    "hover": "HAWAII 22.7%",
+    "url": "https://www.good.is/articles/mapping-america-s-eating-habits", "target": "same_window",
+    "upColor": "#467706", "overColor": "#ECFFB3", "downColor": "#cae9af",
+    "active": true
+  },
+  "usjs12":{
+    "hover": "IDAHO 28.6%",
+    "url": "https://www.good.is/articles/mapping-america-s-eating-habits", "target": "same_window",
+    "upColor": "#c56114", "overColor": "#ECFFB3", "downColor": "#cae9af",
+    "active": true
+  },
+  "usjs13":{
+    "hover": "ILLINOIS 30.8%",
+    "url": "https://www.good.is/articles/mapping-america-s-eating-habits", "target": "same_window",
+    "upColor": "#850d04", "overColor": "#d9ff66", "downColor": "#cae9af",
+    "active": true
+  },
+  "usjs14":{
+    "hover": "INDIANA 31.3%",
+    "url": "https://www.good.is/articles/mapping-america-s-eating-habits", "target": "same_window",
+    "upColor": "#850d04", "overColor": "#ECFFB3", "downColor": "#cae9af",
+    "active": true
+  },
+  "usjs15":{
+    "hover": "IOWA 32.1%",
+    "url": "https://www.good.is/articles/mapping-america-s-eating-habits", "target": "same_window",
+    "upColor": "#850d04", "overColor": "#ECFFB3", "downColor": "#cae9af",
+    "active": true
+  },
+  "usjs16":{
+    "hover": "KANSAS 34.2%",
+    "url": "https://www.good.is/articles/mapping-america-s-eating-habits", "target": "same_window",
+    "upColor": "#850d04", "overColor": "#ECFFB3", "downColor": "#cae9af",
+    "active": true
+  },
+  "usjs17":{
+    "hover": "KENTUCKY 34.6%",
+    "url": "https://www.good.is/articles/mapping-america-s-eating-habits", "target": "same_window",
+    "upColor": "#850d04", "overColor": "#ECFFB3", "downColor": "#cae9af",
+    "active": true
+  },
+  "usjs18":{
+    "hover": "LOUISIANA 36.2%",
+    "url": "https://www.good.is/articles/mapping-america-s-eating-habits", "target": "same_window",
+    "upColor": "#850d04", "overColor": "#ECFFB3", "downColor": "#cae9af",
+    "active": true
+  },
+  "usjs19":{
+    "hover": "MAINE 30.0%",
+    "url": "https://www.good.is/articles/mapping-america-s-eating-habits", "target": "same_window",
+    "upColor": "#850d04", "overColor": "#d9ff66", "downColor": "#cae9af",
+    "active": true
+  },
+  "usjs20":{
+    "hover": "MARYLAND 28.9%",
+    "url": "https://www.good.is/articles/mapping-america-s-eating-habits", "target": "same_window",
+    "upColor": "#c56114", "overColor": "#ECFFB3", "downColor": "#cae9af",
+    "active": true
+  },
+  "usjs21":{
+    "hover": "MASSACHUSETTS 24.3%",
+    "url": "https://www.good.is/articles/mapping-america-s-eating-habits", "target": "same_window",
+    "upColor": "#467706", "overColor": "#ECFFB3", "downColor": "#cae9af",
+    "active": true
+  },
+  "usjs22":{
+    "hover": "MICHIGAN 31.2%",
+    "url": "https://www.good.is/articles/mapping-america-s-eating-habits", "target": "same_window",
+    "upColor": "#850d04", "overColor": "#ECFFB3", "downColor": "#cae9af",
+    "active": true
+  },
+  "usjs23":{
+    "hover": "MINNESOTA 26.1%",
+    "url": "https://www.good.is/articles/mapping-america-s-eating-habits", "target": "same_window",
+    "upColor": "#c56114", "overColor": "#ECFFB3", "downColor": "#cae9af",
+    "active": true
+  },
+  "usjs24":{
+    "hover": "MISSISSIPPI 35.6%",
+    "url": "https://www.good.is/articles/mapping-america-s-eating-habits", "target": "same_window",
+    "upColor": "#850d04", "overColor": "#ECFFB3", "downColor": "#cae9af",
+    "active": true
+  },
+  "usjs25":{
+    "hover": "MISSOURI 32.4%",
+    "url": "https://www.good.is/articles/mapping-america-s-eating-habits", "target": "same_window",
+    "upColor": "#850d04", "overColor": "#d9ff66", "downColor": "#cae9af",
+    "active": true
+  },
+  "usjs26":{
+    "hover": "MONTANA 23.6%",
+    "url": "https://www.good.is/articles/mapping-america-s-eating-habits", "target": "same_window",
+    "upColor": "#467706", "overColor": "#ECFFB3", "downColor": "#cae9af",
+    "active": true
+  },
+  "usjs27":{
+    "hover": "NEBRASKA 31.4%",
+    "url": "https://www.good.is/articles/mapping-america-s-eating-habits", "target": "same_window",
+    "upColor": "#850d04", "overColor": "#ECFFB3", "downColor": "#cae9af",
+    "active": true
+  },
+  "usjs28":{
+    "hover": "NEVADA 26.7%",
+    "url": "https://www.good.is/articles/mapping-america-s-eating-habits", "target": "same_window",
+    "upColor": "#c56114", "overColor": "#ECFFB3", "downColor": "#cae9af",
+    "active": true
+  },
+  "usjs29":{
+    "hover": "NEW HAMPSHIRE 26.3%",
+    "url": "https://www.good.is/articles/mapping-america-s-eating-habits", "target": "same_window",
+    "upColor": "#c56114", "overColor": "#ECFFB3", "downColor": "#cae9af",
+    "active": true
+  },
+  "usjs30":{
+    "hover": "NEW JERSEY 25.6%",
+    "url": "https://www.good.is/articles/mapping-america-s-eating-habits", "target": "same_window",
+    "upColor": "#c56114", "overColor": "#ECFFB3", "downColor": "#cae9af",
+    "active": true
+  },
+  "usjs31":{
+    "hover": "NEW MEXICO 28.8%",
+    "url": "https://www.good.is/articles/mapping-america-s-eating-habits", "target": "same_window",
+    "upColor": "#c56114", "overColor": "#d9ff66", "downColor": "#cae9af",
+    "active": true
+  },
+  "usjs32":{
+    "hover": "NEW YORK 25.0%",
+    "url": "https://www.good.is/articles/mapping-america-s-eating-habits", "target": "same_window",
+    "upColor": "#c56114", "overColor": "#ECFFB3", "downColor": "#cae9af",
+    "active": true
+  },
+  "usjs33":{
+    "hover": "NORTH CAROLINA 30.1%",
+    "url": "https://www.good.is/articles/mapping-america-s-eating-habits", "target": "same_window",
+    "upColor": "#850d04", "overColor": "#ECFFB3", "downColor": "#cae9af",
+    "active": true
+  },
+  "usjs34":{
+    "hover": "NORTH DAKOTA 31.0%",
+    "url": "https://www.good.is/articles/mapping-america-s-eating-habits", "target": "same_window",
+    "upColor": "#850d04", "overColor": "#ECFFB3", "downColor": "#cae9af",
+    "active": true
+  },
+  "usjs35":{
+    "hover": "OHIO 29.8%",
+    "url": "https://www.good.is/articles/mapping-america-s-eating-habits", "target": "same_window",
+    "upColor": "#c56114", "overColor": "#ECFFB3", "downColor": "#cae9af",
+    "active": true
+  },
+  "usjs36":{
+    "hover": "OKLAHOMA 33.9%",
+    "url": "https://www.good.is/articles/mapping-america-s-eating-habits", "target": "same_window",
+    "upColor": "#850d04", "overColor": "#ECFFB3", "downColor": "#cae9af",
+    "active": true
+  },
+  "usjs37":{
+    "hover": "OREGON 30.1%",
+    "url": "https://www.good.is/articles/mapping-america-s-eating-habits", "target": "same_window",
+    "upColor": "#850d04", "overColor": "#ECFFB3", "downColor": "#cae9af",
+    "active": true
+  },
+  "usjs38":{
+    "hover": "PENNSYLVANIA 30.0%",
+    "url": "https://www.good.is/articles/mapping-america-s-eating-habits", "target": "same_window",
+    "upColor": "#850d04", "overColor": "#d9ff66", "downColor": "#cae9af",
+    "active": true
+  },
+  "usjs39":{
+    "hover": "RHODE ISLAND 26.0%",
+    "url": "https://www.good.is/articles/mapping-america-s-eating-habits", "target": "same_window",
+    "upColor": "#c56114", "overColor": "#ECFFB3", "downColor": "#cae9af",
+    "active": true
+  },
+  "usjs40":{
+    "hover": "SOUTH CAROLINA 31.7%",
+    "url": "https://www.good.is/articles/mapping-america-s-eating-habits", "target": "same_window",
+    "upColor": "#850d04", "overColor": "#ECFFB3", "downColor": "#cae9af",
+    "active": true
+  },
+  "usjs41":{
+    "hover": "SOUTH DAKOTA 30.4%",
+    "url": "https://www.good.is/articles/mapping-america-s-eating-habits", "target": "same_window",
+    "upColor": "#850d04", "overColor": "#ECFFB3", "downColor": "#cae9af",
+    "active": true
+  },
+  "usjs42":{
+    "hover": "TENNESSEE 33.8%",
+    "url": "https://www.good.is/articles/mapping-america-s-eating-habits", "target": "same_window",
+    "upColor": "#850d04", "overColor": "#d9ff66", "downColor": "#cae9af",
+    "active": true
+  },
+  "usjs43":{
+    "hover": "TEXAS 32.4%",
+    "url": "https://www.good.is/articles/mapping-america-s-eating-habits", "target": "same_window",
+    "upColor": "#850d04", "overColor": "#ECFFB3", "downColor": "#cae9af",
+    "active": true
+  },
+  "usjs44":{
+    "hover": "UTAH 24.5%",
+    "url": "https://www.good.is/articles/mapping-america-s-eating-habits", "target": "same_window",
+    "upColor": "#467706", "overColor": "#ECFFB3", "downColor": "#cae9af",
+    "active": true
+  },
+  "usjs45":{
+    "hover": "VERMONT 25.1%",
+    "url": "https://www.good.is/articles/mapping-america-s-eating-habits", "target": "same_window",
+    "upColor": "#c56114", "overColor": "#ECFFB3", "downColor": "#cae9af",
+    "active": true
+  },
+  "usjs46":{
+    "hover": "VIRGINIA 29.2%",
+    "url": "https://www.good.is/articles/mapping-america-s-eating-habits", "target": "same_window",
+    "upColor": "#c56114", "overColor": "#ECFFB3", "downColor": "#cae9af",
+    "active": true
+  },
+  "usjs47":{
+    "hover": "WASHINGTON 26.4%",
+    "url": "https://www.good.is/articles/mapping-america-s-eating-habits", "target": "same_window",
+    "upColor": "#c56114", "overColor": "#ECFFB3", "downColor": "#cae9af",
+    "active": true
+  },
+  "usjs48":{
+    "hover": "WEST VIRGINIA 35.6%",
+    "url": "https://www.good.is/articles/mapping-america-s-eating-habits", "target": "same_window",
+    "upColor": "#850d04", "overColor": "#ECFFB3", "downColor": "#cae9af",
+    "active": true
+  },
+  "usjs49":{
+    "hover": "WISCONSIN 30.7%",
+    "url": "https://www.good.is/articles/mapping-america-s-eating-habits", "target": "same_window",
+    "upColor": "#850d04", "overColor": "#ECFFB3", "downColor": "#cae9af",
+    "active": true
+  },
+  "usjs50":{
+    "hover": "WYOMING 29.0%",
+    "url": "https://www.good.is/articles/mapping-america-s-eating-habits", "target": "same_window",
+    "upColor": "#c56114", "overColor": "#ECFFB3", "downColor": "#cae9af",
+    "active": true
+  },
+  "usjs51":{
+    "hover": "WASHINGTON DC 22.1%",
+    "url": "https://www.good.is/articles/mapping-america-s-eating-habits", "target": "same_window",
+    "upColor": "#467706", "overColor": "#ECFFB3", "downColor": "#cae9af",
+    "active": true
+  },
+  "general":{
+    "borderColor": "#9CA8B6",
+    "visibleNames": "#adadad",
+    "lakesFill": "#C5E8FF",
+    "lakesOutline": "#9CA8B6"
+  }
+};
